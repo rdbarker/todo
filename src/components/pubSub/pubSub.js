bf1fdd60647eb09412534f681bd1d98913498316
rdbarker <ryan.dean.barker@gmail.com>
2021-11-05 18:53:50 -0400

    formatting

--- src/components/pubSub/pubSub.js
+++ src/components/pubSub/pubSub.js
@@ -1,24 +1,24 @@
-const subscriptionService = (() => {
-    const subs = {};
-    const subscribe = (topic,callBack) =>{
-        if (!subs.hasOwnProperty(topic)){
-            subs[topic]=[];
-        }
-        subs[topic].push(callBack);
+const subscriptionService = () => {
+  const subs = {};
+  const subscribe = (topic, callBack) => {
+    if (!subs.hasOwnProperty(topic)) {
+      subs[topic] = [];
     }
-    const publish = (topic,data) => {
-        subs[topic].forEach(subCallBack => subCallBack(data));
+    subs[topic].push(callBack);
+  };
+  const publish = (topic, data) => {
+    subs[topic].forEach((subCallBack) => subCallBack(data));
+  };
+  const unsubscribe = (topic, callBack) => {
+    const index = subs[topic].indexOf(callBack);
+    if (index > -1) {
+      subs[topic].splice(index, 1);
     }
-    const unsubscribe = (topic,callBack) =>{
-        const index = subs[topic].indexOf(callBack);
-        if (index > -1){
-            subs[topic].splice(index, 1);
-        }
-        if (subs[topic].length===0){
-            delete subs[topic];
-        }
+    if (subs[topic].length === 0) {
+      delete subs[topic];
     }
-    return {subscribe,publish,unsubscribe}
-})();
+  };
+  return { subscribe, publish, unsubscribe };
+};
 
 export default subscriptionService;
