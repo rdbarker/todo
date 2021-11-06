a769994002e41cb67d6d4b345c7b24fec009d9af
rdbarker <ryan.dean.barker@gmail.com>
2021-11-05 21:27:10 -0400

    basic fortmat

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import pubSub from "./components/pubSub/pubSub.js";
+import dataController from "./components/dataController/dataController.js";
+import sideMenu from "./views/sideMenu.js";
+
+const logger = (data) => {
+  console.log(data);
+};
+const service = pubSub();
+const data = dataController(service);
+const side = sideMenu(service);
+data.createProject({ title: "NewTitle", description: "NewDescription" });
+data.createProject({ title: "NewTitle2", description: "NewDescription2" });
+data.createProject({ title: "NewTitle3", description: "NewDescription3" });
+data.createProject({ title: "NewTitle4", description: "NewDescription4" });
+console.log(data.getAllProjectTitles());
+data.createProject();
