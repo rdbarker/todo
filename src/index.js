190290bd6739c57174397619a4e0ca04b74105e5
rdbarker <ryan.dean.barker@gmail.com>
2021-11-05 22:23:22 -0400

    remove debugging stuff

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import pubSub from "./components/pubSub/pubSub.js";
 import dataController from "./components/dataController/dataController.js";
 import sideMenu from "./views/sideMenu.js";
 
-const logger = (data) => {
-  console.log(data);
-};
 const service = pubSub();
 const data = dataController(service);
 const side = sideMenu(service);
@@ -12,5 +9,3 @@ data.createProject({ title: "NewTitle", description: "NewDescription" });
 data.createProject({ title: "NewTitle2", description: "NewDescription2" });
 data.createProject({ title: "NewTitle3", description: "NewDescription3" });
 data.createProject({ title: "NewTitle4", description: "NewDescription4" });
-console.log(data.getAllProjectTitles());
-data.createProject();
