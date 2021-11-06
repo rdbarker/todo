b1645483d43ab45874646da5b7f4b759ac0d6f23
rdbarker <ryan.dean.barker@gmail.com>
2021-11-05 20:40:06 -0400

    added template for html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,37 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "ToDo",
-        })
+  mode: "development",
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "ToDo",
+      template: "./src/index.html",
+    }),
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
-    devtool: "inline-source-map",
-    devServer:{
-        static: './dist'
-    },
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource"
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: "asset/resource"
-            }
-        ]        
-    }
+  },
 };
