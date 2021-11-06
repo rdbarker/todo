6a661255a44444c72ff4c639978896302e9181e5
rdbarker <ryan.dean.barker@gmail.com>
2021-11-05 22:24:19 -0400

    add button creates new project(debug)

--- src/views/sideMenu.js
+++ src/views/sideMenu.js
@@ -3,7 +3,16 @@ import sideMenuBottom from "./subViews/sideMenuBottom";
 import sideMenuTop from "./subViews/sideMenuTop";
 
 const sideMenu = (dataService) => {
+  const addProject = () => {
+    dataService.publish("createProject");
+  };
   const $parent = createElement({ parent: "#container", css: "side-menu" });
+  const $addButton = createElement({
+    parent: $parent,
+    tag: "button",
+    inner: "add",
+    click: addProject,
+  });
   sideMenuTop($parent);
   sideMenuBottom(dataService, $parent);
 };
